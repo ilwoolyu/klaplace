5322ce09e305dbb0e59b42ce30605838feb7f740
ilwoolyu <ilwoolyu@gmail.com>
2020-02-27 22:52:53 -0600

    Remove unnecessary options/outputs/vxl; support VTK8

--- vtkio.h
+++ vtkio.h
@@ -10,7 +10,6 @@
 #define __ktools__vtkio__
 
 #include <iostream>
-#include <vnl/vnl_matrix.h>
 
 class vtkPolyData;
 class vtkDataSet;
@@ -21,10 +20,6 @@ class vtkIO {
 public:
     /// @brief Rotate the object around z-axis. Change the sign of x and y coordinates. This works in place.
     void zrotate(vtkPolyData* p);
-
-    /// @brief Create a rotation matrix that rotates v1 to v2
-    void rotateVector(const double* v1, const double* v2, vnl_matrix<double>& rotation);
-
 
 	vtkDataSet* readDataFile(std::string file);
     vtkPolyData* readFile(std::string file);
